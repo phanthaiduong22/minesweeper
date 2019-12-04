daf1a6149543e9c7f944a4f29d7bd62ceb3aa239
Duong <30502963+phanthaiduong22@users.noreply.github.com>
2019-12-04 18:45:58 +0700

    third commit

--- time.js
+++ time.js
@@ -16,7 +16,6 @@ function add() {
     h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
     if (canClickCanvas == true)
         timer();
-    console.log(canClickCanvas);
 }
 timer();
 function timer() {
