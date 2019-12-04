ca579d82ac9e23186d006972a6dcc7a6582f4e96
Duong <30502963+phanthaiduong22@users.noreply.github.com>
2019-12-04 18:44:57 +0700

    second commit

--- app.js
+++ app.js
@@ -166,8 +166,6 @@ function newGame() {
     clearTimeout(t);
     h1.textContent = "00:00:00";
     seconds = 0; minutes = 0; hours = 0;
-    timer();
-    console.log(seconds);
     document.querySelector(".showGameOver").textContent = "";
     document.querySelector(".showScore").textContent = 0;
     var x = document.getElementById("levelSelector").value;
